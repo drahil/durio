be76cc563da6e2fbbfd1459e51c1de305c8ef3db
Omar Djecevic <djecevic.omar@gmail.com>
2023-06-25 12:10:50 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>Durio</title>
+
+        @vite('resources/css/app.css')
+    </head>
+    <body >
+
+    </body>
+</html>
